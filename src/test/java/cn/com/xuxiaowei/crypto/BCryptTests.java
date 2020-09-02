fed59a5f9e3a4ea9a1b764a42c5cdc67ecd96043
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-02 10:17:11 +0800

    验证带有前缀的 BCrypt 加密结果

--- src/test/java/cn/com/xuxiaowei/crypto/BCryptTests.java
+++ src/test/java/cn/com/xuxiaowei/crypto/BCryptTests.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.crypto;
 
 import org.junit.Test;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  * 测试密码加密
@@ -9,16 +10,40 @@ import org.junit.Test;
  */
 public class BCryptTests {
 
+    /**
+     * BCrypt 加密及验证
+     */
     @Test
     public void hashpw() {
         String password = "xxw";
         System.out.println("原密码：\t" + password);
 
-        String hashed = org.mindrot.jbcrypt.BCrypt.hashpw(password, org.mindrot.jbcrypt.BCrypt.gensalt());
+        String hashed = BCrypt.hashpw(password, org.mindrot.jbcrypt.BCrypt.gensalt());
         System.out.println("加密后：\t" + hashed);
 
-        boolean checkpw = org.mindrot.jbcrypt.BCrypt.checkpw(password, hashed);
+        boolean checkpw = BCrypt.checkpw(password, hashed);
         System.out.println("验证：\t" + checkpw);
+    }
+
+    /**
+     * 验证带有前缀的 BCrypt 加密结果
+     */
+    @Test
+    public void securityBcrypt() {
+        String password = "xxw";
+        System.out.println("原密码：\t\t\t\t\t\t\t" + password);
+
+        String prefixHashed = "{bcrypt}$2a$10$bCMT61vI.XEYZnTILyqKq.nce8a3B1WUHOIJMZUru2ducZytnIRDi";
+        System.out.println("带有前缀的 BCrypt 加密后：\t\t\t" + prefixHashed);
+
+        String hashed = "$2a$10$bCMT61vI.XEYZnTILyqKq.nce8a3B1WUHOIJMZUru2ducZytnIRDi";
+        System.out.println("BCrypt 加密后：\t\t\t\t\t" + hashed);
+
+        boolean prefixCheckpw = BCryptPasswordEncoder.checkpw(password, prefixHashed);
+        System.out.println("验证带有前缀的 BCrypt 加密后：\t\t" + prefixCheckpw);
+
+        boolean checkpw = BCrypt.checkpw(password, hashed);
+        System.out.println("验证 BCrypt 加密后：\t\t\t\t" + checkpw);
     }
 
 }
